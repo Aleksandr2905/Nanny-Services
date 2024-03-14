c28afae79ccc1de190df917655c5d2f7cd7435d1
Aleksandr2905 <natalukha@ukr.net>
2024-03-14 22:42:58 +0200

    added firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/Home/HomePage";
 import { NanniesPage } from "./pages/Nannies/NanniesPage";
 import { FavoritesPage } from "./pages/Favorites/FavoritesPage";
+import { Layout } from "./components/Layout/Layout";
 
-function App() {
+const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/nannies" element={<NanniesPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-      <Route path="*" element={<HomePage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/nannies" element={<NanniesPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<HomePage />} />
+      </Route>
     </Routes>
   );
-}
+};
 
 export default App;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,13 @@
 import { Suspense } from "react";
-import { Header } from "../Header/Header";
 import { Outlet } from "react-router-dom";
 import { Loader } from "../Loader/Loader";
 
 export const Layout = () => {
   return (
-    <>
-      <Header backgroundColor="transparent" />
-      <main>
-        <Suspense fallback={<Loader />}>
-          <Outlet />
-        </Suspense>
-      </main>
-    </>
+    <main>
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </main>
   );
 };
