6246c38ccc1dbd12cd26c2054bc7e62cf46ea1da
Aleksandr2905 <natalukha@ukr.net>
2024-03-14 17:10:09 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/Home/HomePage";
+import { NanniesPage } from "./pages/Nannies/NanniesPage";
+import { FavoritesPage } from "./pages/Favorites/FavoritesPage";
+
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/nannies" element={<NanniesPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
   );
 }
 
