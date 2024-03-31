be411ffc329decb721b1a2a678e8e92a62203694
Aleksandr2905 <natalukha@ukr.net>
2024-03-31 23:13:12 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,24 +3,25 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const {
-  VITE_FIREBASE_API_KEY,
-  VITE_AUTH_DOMAIN,
-  VITE_DATABASE_URL,
-  VITE_PROJECT_ID,
-  VITE_STORAGE_BUCKET,
-  VITE_MESSAGING_SENDER_ID,
-  VITE_APP_ID,
-} = import.meta.env;
+// const {
+//   VITE_FIREBASE_API_KEY,
+//   VITE_AUTH_DOMAIN,
+//   VITE_DATABASE_URL,
+//   VITE_PROJECT_ID,
+//   VITE_STORAGE_BUCKET,
+//   VITE_MESSAGING_SENDER_ID,
+//   VITE_APP_ID,
+// } = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: VITE_FIREBASE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  databaseURL: VITE_DATABASE_URL,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BUCKET,
-  messagingSenderId: VITE_MESSAGING_SENDER_ID,
-  appId: VITE_APP_ID,
+  apiKey: "AIzaSyCLHUUo4322dJgd4kpVNHcE7eJHi4IhKZw",
+  authDomain: "nanny-services-afa99.firebaseapp.com",
+  databaseURL:
+    "https://nanny-services-afa99-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "nanny-services-afa99",
+  storageBucket: "nanny-services-afa99.appspot.com",
+  messagingSenderId: "25935499254",
+  appId: "1:25935499254:web:b43e1d1ccfd694c85d41d7",
 };
 
 const app = initializeApp(firebaseConfig);
