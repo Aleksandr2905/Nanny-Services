46f374bfcd632f9938d11eb6a986e55b7d25ed2d
Aleksandr2905 <natalukha@ukr.net>
2024-04-08 21:06:52 +0300

    fix favorites page

--- src/pages/Favorites/FavoritesPage.jsx
+++ src/pages/Favorites/FavoritesPage.jsx
@@ -1,18 +1,17 @@
 import { useSelector } from "react-redux";
 import { Header } from "../../components/Header/Header";
 import { selectIsAuth } from "../../redux/auth/selectors";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { FavoritesList } from "../../components/FavoritesList/FavoritesList";
 
 export const FavoritesPage = () => {
   const navigation = useNavigate();
-  const location = useLocation();
   const isAuth = useSelector(selectIsAuth);
 
   useEffect(() => {
     if (!isAuth) {
-      navigation("/" && location.pathname);
+      navigation("/");
     }
   }, [isAuth, navigation]);
 
